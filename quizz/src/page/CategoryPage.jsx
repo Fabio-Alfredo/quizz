267a0412ca6d6b79709d876287aaf32aafb5210f
Fabio-Alfredo <00210121@uca.edu.sv>
2024-04-06 14:27:07 -0600

    add: container questions

--- quizz/src/page/CategoryPage.jsx
+++ quizz/src/page/CategoryPage.jsx
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { questions } from '../data';
+
+const shuffleArray = (array) => {
+
+    const newArray = array.sort(() => Math.random() - 0.5)
+    return newArray.slice(0, 5)
+}
+
 
 export const CategoryPage = () => {
-    
-    const{category}=useParams()
+
+    const { category } = useParams()
+
+    const [questionFiltered, setQuestionFiltered] = useState(
+        questions.filter(question => question.category === category)
+    )
+
+    useEffect(() => {
+        const newQuestions = shuffleArray(questionFiltered)
+        setQuestionFiltered(newQuestions)
+    }, [])
 
     return (
-        <div>
-            <h1>categoria</h1>
+        <div className='container flex flex-col items-center justify-center gap-10'
+            style={{ height: 'calc(100vh - 5rem)' }}
+        >
+            page
         </div>
     );
 };
